f9cfbd164bcff6d867ce33bf3bbdb9803dc9b72b
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2023-12-31 12:30:10 +0200

    fix trendings posts resolver

---
+++ graphql/resolvers/post/trendingPostsResolver.js
@@ -0,0 +1,42 @@
+const db = require("../../../models");
+
+const trendingsPostsResolver = async () => {
+  try {
+    const twentyFourHoursAgo = new Date(); 
+    twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24);
+
+    const trendingPostIdsAndCount = await db.sequelize.query(
+      `
+      SELECT 
+        Post.post_id,
+        IFNULL(COUNT(UserPostInteractions.post_id), 0) AS interaction_count
+      FROM 
+        Posts AS Post
+      LEFT JOIN 
+        UserPostInteractions ON Post.post_id = UserPostInteractions.post_id AND UserPostInteractions.createdAt >= :twentyFourHoursAgo
+      GROUP BY 
+        Post.post_id, Post.user_id, Post.parent_post_id, Post.text, Post.timestamp
+      ORDER BY 
+        interaction_count DESC
+      LIMIT 50
+      `,
+      {
+        type: db.sequelize.QueryTypes.SELECT,
+        replacements: {
+          twentyFourHoursAgo: twentyFourHoursAgo.toISOString(),
+        },
+      }
+    );
+
+    const trendingPosts = await Promise.all(trendingPostIdsAndCount.map(async ({ post_id }) => {
+      return await db.Post.findByPk(post_id)
+    }))
+
+    return trendingPosts;
+  } catch (error) {
+    console.error("Error fetching trending posts:", error);
+    throw error;
+  }
+}
+
+module.exports = trendingsPostsResolver;
